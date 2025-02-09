06899937c2d2f8c9f4c05ef4488f0c0ea3a858d9
nkatsitadze <nkatsitadze@identomat.com>
2025-02-09 21:10:24 +0400

    await error v4

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,17 @@
-export default function Product({ params }: { params: Record<string, string> }) {
-    return <h5>{params.id}</h5>;
+import { Metadata } from "next";
+
+type ProductPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: ProductPageProps): Promise<Metadata> {
+  return {
+    title: `Product ${params.id}`,
+  };
+}
+
+export default function Product({ params }: ProductPageProps) {
+  return <h5>{params.id}</h5>;
 }
